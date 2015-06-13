e9c75c6776d0b08e4e3c0a9c9676df44f703ae93
elc1798 <subliminalmau5@gmail.com>
2015-06-13 18:35:10 -0400

    Add Scout implementation

--- com/cavernwars/entities/aboveground/Scout.java
+++ com/cavernwars/entities/aboveground/Scout.java
@@ -1,5 +1,70 @@
 package com.cavernwars.entities.aboveground;
 
-public class Scout {
+import com.cavernwars.Controller;
+import com.cavernwars.entities.Entity;
 
+/*
+ * Scouts are "AboveGround" or "AG" units.
+ * Cost: 150 + 60 * Level
+ * Stats:
+ * Health: 2 + Level
+ * Speed: 2
+ * Damage: 2 + Level
+ *
+ * Special:
+ * This unit is range, depends on it's level.
+ */
+public class Scout extends Entity {
+
+    private Controller session;
+    private int[][] path;
+
+    private int pathCounter = 0;
+
+    public Scout(Controller c) {
+        session = c;
+        /*
+         * Each element in 'path' is formated like such:
+         * {xcoor to start at , ycoor to start at , seconds to reach next destination}
+         * -1 is used to denote an infinite amount of time.
+         */
+        path = Entity.AGPATH;
+        this.setSprite("/resources/TestSprite.png");
+        this.setX(path[0][0]);
+        this.setY(path[0][1]);
+        this.setMaxHealth((int)session.AGLevels[4] + 2);
+        this.setHealth((int)session.AGLevels[4] + 2);
+        this.setSpeed(2);
+        this.setDamage((int)session.AGLevels[4] + 2);
+    }
+
+    @Override
+    public void move() {
+        if (pathCounter + 1 < path.length && Math.abs((double) getX() - path[pathCounter +1][0]) < 5.0 && Math.abs((double) getX() - path[pathCounter +1][0]) < 5.0) {
+            setX(path[pathCounter + 1][0]);
+            setY(path[pathCounter + 1][1]);
+        }
+        // Out of bounds?? This is a hacky fix :D
+        if (getX() < 0) {
+            setX(5);
+        }
+        if (getX() > 515) {
+            setX(500);
+        }
+        if (pathCounter + 1 < path.length && getX() == path[pathCounter + 1][0] && getY() == path[pathCounter + 1][1]) {
+            pathCounter++;
+        }
+        if (path[pathCounter][2] != -1) {
+            if (path[pathCounter][2] == 0) {
+                setX(path[pathCounter + 1][0]);
+                setY(path[pathCounter + 1][1]);
+            } else {
+                int dx , dy;
+                dx = (path[pathCounter + 1][0] - path[pathCounter][0]) / path[pathCounter][2] * getSpeed() / 10;
+                dy = (path[pathCounter + 1][1] - path[pathCounter][1]) / path[pathCounter][2] * getSpeed() / 10;
+                setX(getX() + dx);
+                setY(getY() + dy);
+            }
+        }
+    }
 }
