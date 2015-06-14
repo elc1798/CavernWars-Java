ef56db3269d57387b3457c5b50b15f47608d7a09
elc1798 <subliminalmau5@gmail.com>
2015-06-14 17:11:43 -0400

    Add button presses

--- com/cavernwars/GUI/MouseInterpreter.java
+++ com/cavernwars/GUI/MouseInterpreter.java
@@ -11,12 +11,18 @@ public class MouseInterpreter implements MouseListener {
 
     private Controller session;
     private Rectangle cursor;
+    private Rectangle trapzone;
     private int buttonHoverID;
+    private boolean placing;
+    private int placingID;
 
     public MouseInterpreter(Controller session) {
         this.session = session;
         cursor = new Rectangle(mouseX() , mouseY() , 2 , 2);
         buttonHoverID = -1;
+        placing = false;
+        placingID = -1;
+        trapzone = new Rectangle(5 , 540 , 455 , 55);
     }
 
     public int mouseX() {
@@ -51,7 +57,65 @@ public class MouseInterpreter implements MouseListener {
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        System.out.printf("%3d %3d\n" , e.getX() , e.getY());
+        if (!placing) {
+            switch(updateMouseLocation()) {
+            case 0:
+                if (session.UGLevels[1] > 5.0 && session.UGLevels[2] > 5.0 && session.UGLevels[3] > 5.0 && session.UGLevels[4] > 5.0) {
+                    if (session.playerMoney > 1000 + 100 * (int)session.UGLevels[0]) {
+                        session.b0.spawn();
+                    }
+                }
+                break;
+            case 1:
+                if (session.playerMoney > 150 + 60 * (int)session.UGLevels[1]) {
+                    session.b1.spawn();
+                }
+                break;
+            case 2:
+                if (session.playerMoney > 100 + 50 * (int)session.UGLevels[2]) {
+                    session.b2.spawn();
+                }
+                break;
+            case 3:
+                if (session.playerMoney > 75 + 25 * (int)session.UGLevels[3]) {
+                    session.b3.spawn();
+                }
+                break;
+            case 4:
+                if (session.playerMoney > 120 + 40 * (int)session.UGLevels[4]) {
+                    session.b4.spawn();
+                }
+                break;
+            case 5:
+                if (session.playerMoney > 1000) {
+                    placingID = 5;
+                    placing = true;
+                }
+                break;
+            case 6:
+                if (session.playerMoney > 3000) {
+                    placingID = 6;
+                    placing = true;
+                }
+                break;
+            case -1:
+                // Do nothing
+                break;
+            default:
+                // Do nothing
+                break;
+            }
+        } else {
+            placing = false;
+            cursor = new Rectangle(mouseX() , mouseY() , 2 , 2);
+            if (cursor.intersects(trapzone)) {
+                if (placingID == 5) {
+                    session.b5.spawn(mouseX() , 550);
+                } else if (placingID == 6) {
+                    session.b6.spawn(mouseX() , 550);
+                }
+            }
+        }
     }
 
     @Override
